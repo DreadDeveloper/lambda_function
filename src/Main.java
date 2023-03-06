5df503ec534ccc4127576754ba015850599c2bb2
Abhinav Kumar <kumaraka2000@gmail.com>
2023-03-06 22:03:57 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 import java.util.function.Function;
 public class Main
 {
 	public static void main(String[] args)
 	{
 		Function<Integer, Integer> function = integer -> integer * integer;
-		int number = 8;
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
 		System.out.println(function.apply(number));
 	}
 }

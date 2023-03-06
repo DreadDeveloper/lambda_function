5a4a997fce81296a7946415d20e65553baff07ec
Abhinav Kumar <kumaraka2000@gmail.com>
2023-03-06 22:03:07 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.function.Function;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Function<Integer, Integer> function = integer -> integer * integer;
+		int number = 8;
+		System.out.println(function.apply(number));
+	}
 }
